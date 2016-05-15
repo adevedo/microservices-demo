fd94d5bdff27c6a1a6a2105ca1bafc9cf1f31ec5
adevedo <adevedo@gmail.com>
2016-05-15 12:19:10 +0400

    change

--- party-management-webservice/src/main/java/ae/co/etisalat/cbcm/services/parymanagement/FilteredRestWs.java
+++ party-management-webservice/src/main/java/ae/co/etisalat/cbcm/services/parymanagement/FilteredRestWs.java
@@ -1,5 +1,10 @@
 package ae.co.etisalat.cbcm.services.parymanagement;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.consul.discovery.ConsulDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,9 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FilteredRestWs {
 
+	@Autowired
+	private ConsulDiscoveryClient discoveryClient;
+
 	@RequestMapping(value = "/hello-world", method = RequestMethod.GET, produces = "application/json")
 	public @ResponseBody String index(@RequestParam(value = "fields", required = false) String fields) {
-		return "Hello World";
+		List<ServiceInstance> instances = discoveryClient.getInstances("party-management-webservice");
+		String res = "";
+		for (ServiceInstance ins : instances) {
+			res += ins.getUri() + ":" + ins.getHost() + "\r\n";
+		}
+		return res;
 	}
 
 }
